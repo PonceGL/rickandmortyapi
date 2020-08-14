bf318b6ac2d69bf8915da790b2d307e982d236f5
Ponce Guevara <poncianogl@hotmail.com>
2020-08-14 11:37:37 -0500

    La segunda peticion en ciclo, aun no funciona

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
 const API = 'https://rickandmortyapi.com/api/character/';
-const API_LOCATION = 'https://rickandmortyapi.com/api/location/';
+const API_LOCATION = `https://rickandmortyapi.com/api/location/1`;
 const container = document.getElementById('container');
 
 const fetchData = (url_api, api_dimension) => {
@@ -18,28 +18,78 @@ const fetchData = (url_api, api_dimension) => {
     });
 }
 
-const anotherFunction = async (url_api, api_dimension) => {
-    try{
-        const data = await fetchData(url_api, api_dimension);
-        const dimensions = await fetchData(api_dimension);
-        
-        function print(){
-            for (let i = 0; i < data.results.length; i++) {
-                container.innerHTML += `
-                    <div class="character">
-                        <img src="${data.results[i].image}" alt="">
-                        <div class="container-name">
-                            <h2>${data.results[i].name}</h2>
-                        </div>
+fetchData(API)
+    .then(data => {
+        console.log(data.info.count);
+        for (var i = 0; i < data.results.length; i++) {
+            container.innerHTML += `
+                <div class="character">
+                    <img src="${data.results[i].image}" alt="">
+                    <div class="container-name">
+                        <h2>${data.results[i].name}</h2>
+                        <h2>${data.results[i].name}</h2>
+                        <p>${data.results[i].species}</p>
+                        <p>${data.results[i].gender}</p>
+                        <p>${data.results[i].status}</p>
+                        <p>${data.results[i].type}</p>
+                        <p>${data.results[i].origin.name}</p>
+                        <p>${data.results[i].dimension}</p>
                     </div>
-                `;
-            }
+                </div>
+            `;
+            console.log(data.results[i].origin.url);
         }
-        print();
-    
-    }catch(error){
-        console.error(error)
-    }
-}
+    })
+    // .then(data => {
+    //     console.log(data.name);
+    //     return fetchData(data.origin.url)
+    // })
+    // .then(data => {
+    //     console.log(data.dimension);
+    // })
+    .catch(err => console.error(err));
 
-anotherFunction(API, API_LOCATION);
+
+
+
+
+
+
+
+
+
+
+// const anotherFunction = async (url_api, api_dimension) => {
+//     try{
+//         const data = await fetchData(url_api, api_dimension);
+//         // const character = await fetchData(`${API}${data.results[0].id}`);
+//         // const dimensions = await fetchData(character.origin.url);
+        
+//         function print(){
+//             for (let i = 0; i < data.results.length; i++) {
+//                 container.innerHTML += `
+//                     <div class="character">
+//                         <img src="${data.results[i].image}" alt="">
+//                         <div class="container-name">
+//                             <h2>${data.results[i].name}</h2>
+//                             <h2>${data.results[i].name}</h2>
+//                             <p>${data.results[i].species}</p>
+//                             <p>${data.results[i].gender}</p>
+//                             <p>${data.results[i].status}</p>
+//                             <p>${data.results[i].type}</p>
+//                             <p>${data.results[i].origin.name}</p>
+//                             <p>${data.results[i].dimension}</p>
+//                         </div>
+//                     </div>
+//                 `;
+//                 console.log(data.results[i].origin.url);
+//             }
+//         }
+//         print();
+    
+//     }catch(error){
+//         console.error(error)
+//     }
+// }
+
+// anotherFunction(API, API_LOCATION);
