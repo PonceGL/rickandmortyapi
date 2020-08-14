9afdc6a2b79e0833ca0ae35bfa9793ebce4616a9
Ponce Guevara <poncianogl@hotmail.com>
2020-08-14 18:43:24 -0500

    Listo! pero sin dimension

--- static/js/index.js
+++ static/js/index.js
@@ -1,16 +1,15 @@
 const API = 'https://rickandmortyapi.com/api/character/';
-const API_LOCATION = `https://rickandmortyapi.com/api/location/1`;
 const container = document.getElementById('container');
 
-const fetchData = (url_api, api_dimension) => {
+const fetchData = (url_api) => {
     return new Promise((resolve, reject) => {
         const xhttp = new XMLHttpRequest();
-        xhttp.open('GET', url_api, api_dimension, true); // true es por defecto y activa el asincronismo
+        xhttp.open('GET', url_api, true); // true es por defecto y activa el asincronismo
         xhttp.onreadystatechange = (() => {
             if(xhttp.readyState === 4){ // solicitud finalizada y respuesta lista
                 (xhttp.status === 200) // if ternario
                     ? resolve(JSON.parse(xhttp.responseText))
-                    : reject(new Error('Error ' + url_api, api_dimension))
+                    : reject(new Error('Error ' + url_api))
             }
         })
     
@@ -20,76 +19,22 @@ const fetchData = (url_api, api_dimension) => {
 
 fetchData(API)
     .then(data => {
-        console.log(data.info.count);
-        for (var i = 0; i < data.results.length; i++) {
+        //console.log(data.info.count);
+        for (let i = 0; i < data.results.length; i++) {
+            let dimension = data.results[i].origin.url;
             container.innerHTML += `
-                <div class="character">
-                    <img src="${data.results[i].image}" alt="">
+                <div class="character" data-dimension="${dimension}">
+                    <img class="character-img" src="${data.results[i].image}" alt="">
                     <div class="container-name">
-                        <h2>${data.results[i].name}</h2>
                         <h2>${data.results[i].name}</h2>
                         <p>${data.results[i].species}</p>
                         <p>${data.results[i].gender}</p>
                         <p>${data.results[i].status}</p>
                         <p>${data.results[i].type}</p>
                         <p>${data.results[i].origin.name}</p>
-                        <p>${data.results[i].dimension}</p>
                     </div>
                 </div>
             `;
-            console.log(data.results[i].origin.url);
         }
     })
-    // .then(data => {
-    //     console.log(data.name);
-    //     return fetchData(data.origin.url)
-    // })
-    // .then(data => {
-    //     console.log(data.dimension);
-    // })
     .catch(err => console.error(err));
-
-
-
-
-
-
-
-
-
-
-
-// const anotherFunction = async (url_api, api_dimension) => {
-//     try{
-//         const data = await fetchData(url_api, api_dimension);
-//         // const character = await fetchData(`${API}${data.results[0].id}`);
-//         // const dimensions = await fetchData(character.origin.url);
-        
-//         function print(){
-//             for (let i = 0; i < data.results.length; i++) {
-//                 container.innerHTML += `
-//                     <div class="character">
-//                         <img src="${data.results[i].image}" alt="">
-//                         <div class="container-name">
-//                             <h2>${data.results[i].name}</h2>
-//                             <h2>${data.results[i].name}</h2>
-//                             <p>${data.results[i].species}</p>
-//                             <p>${data.results[i].gender}</p>
-//                             <p>${data.results[i].status}</p>
-//                             <p>${data.results[i].type}</p>
-//                             <p>${data.results[i].origin.name}</p>
-//                             <p>${data.results[i].dimension}</p>
-//                         </div>
-//                     </div>
-//                 `;
-//                 console.log(data.results[i].origin.url);
-//             }
-//         }
-//         print();
-    
-//     }catch(error){
-//         console.error(error)
-//     }
-// }
-
-// anotherFunction(API, API_LOCATION);
